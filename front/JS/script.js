96bb4fcf0348ecd520bd53a7e78df8f29ac567f0
toplev <topourialevan@gmail.com>
2022-11-08 23:42:32 +0100

    update

--- front/JS/script.js
+++ front/JS/script.js
@@ -1,10 +1,22 @@
+const news = document.getElementById("new");
+
 fetch("http://localhost:3000/api/products")
   .then((response) => {
     return response.json();
   })
   .then((data) => {
-    data.forEach((id) => {
-      console.log(id.name);
+    data.forEach((data) => {
+      news.innerHTML = data.name;
+      console.log(data.name);
     });
+  })
+  .catch((error) => console.log(error));
+
+const quote = document.getElementById("quote");
+
+fetch("http://api.quotable.io/random")
+  .then((res) => res.json())
+  .then((data) => {
+    quote.innerHTML = data.content;
   })
   .catch((error) => console.log(error));
