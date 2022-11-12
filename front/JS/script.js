6449bd1e39d3ccc19a1f4d16c3661b212516b9f0
toplev <topourialevan@gmail.com>
2022-11-12 23:28:20 +0100

    update

--- front/JS/script.js
+++ front/JS/script.js
@@ -1,4 +1,4 @@
-const news = document.getElementById("new");
+/*const items = document.getElementById("items");
 
 fetch("http://localhost:3000/api/products")
   .then((response) => {
@@ -6,17 +6,84 @@ fetch("http://localhost:3000/api/products")
   })
   .then((data) => {
     data.forEach((data) => {
-      news.innerHTML = data.name;
+      items.innerHTML = data.name;
+      let li = document.createElement("li");
+      li.textContent = data.name;
       console.log(data.name);
     });
   })
   .catch((error) => console.log(error));
 
-const quote = document.getElementById("quote");
-
-fetch("http://api.quotable.io/random")
+const items = document.getElementById("items");
+fetch("http://localhost:3000/api/products")
   .then((res) => res.json())
   .then((data) => {
-    quote.innerHTML = data.content;
-  })
-  .catch((error) => console.log(error));
+    console.log(data[0].imageUrl);
+    items.innerHTML = data[0].imageUrl;
+  });
+
+for (i = 0; i < items.length; i++) {
+  console.log(typeof item[i]);
+}*/
+
+const items = document.getElementById("items");
+
+fetch("http://localhost:3000/api/products")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    document.getElementById("items").innerHTML = data[0].name;
+  });
+
+/* const items = () => {
+  fetch("http://localhost:3000/api/products")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      items.innerHTML = data;
+    })
+    .catch((error) => console.log(error));
+};
+
+for (i = 0; i < quote.length; i++) {
+  console.log(typeof quote[i]);
+}
+
+getitems();
+ users.forEach(function (user) {
+  let li = document.createElement("li");
+  li.classList.add("list-group-item");
+  li.textContent = user.name;
+  document.querySelector(".list-group").appendChild(li);
+  console.log(user.name);
+});
+
+const item = [
+  fetch("http://localhost:3000/api/products")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      items.forEach(function (item) {
+        console.log(data);
+        let li = document.createElement("li");
+        li.classList.add("list-group-item");
+        li.textContent = data.name;
+        document.querySelector(".list-group").appendChild(li);
+      });
+    }),
+];
+
+const ite = [
+  fetch("http://localhost:3000/api/products")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data[0].name);
+    }),
+];
+
+for (i = 0; i < items.length; i++) {
+  console.log(typeof items[i]);
+}*/
