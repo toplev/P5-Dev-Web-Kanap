13a36c582c2dbe6ac3e4f16439fecf252ae4542a
toplev <topourialevan@gmail.com>
2022-12-03 23:52:46 +0100

    update

--- front/JS/cart.js
+++ front/JS/cart.js
@@ -7,9 +7,20 @@ fetch("http://localhost:3000/api/products/")
   .then((data) => {
     console.log(data);
 
-    const produits = document.getElementById("cart__items");
-    const cart__items = document.createElement("article");
+    const items = document.getElementById("cart__items");
+    const cart__article = document.createElement("article");
+    cart__article.classList.add("cart__item");
+    cart__article.setAttribute("data-id", obj.Id);
+    cart__article.setAttribute("data-color", obj.color);
+    items.appendChild(cart__article);
   })
   .catch(function (err) {
     // Une erreur est survenue
   });
+
+// Retrieving data:
+let color = localStorage.getItem("Panier");
+let obj = JSON.parse(color);
+/*document.getElementById("data-id").innerHTML = obj.Id;
+document.getElementById("data-color").innerHTML = obj.color;*/
+document.getElementById("demo").innerHTML = obj.quantity;
