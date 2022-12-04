6ec9381ed725d038cd024a9e81a38d812b97bf56
toplev <topourialevan@gmail.com>
2022-12-04 14:43:58 +0100

    update

--- front/JS/cart.js
+++ front/JS/cart.js
@@ -1,3 +1,12 @@
+fetch("http://localhost:3000/api/products/" + id).then((res) => {
+  if (res.ok) {
+    return res.json();
+  }
+});
+.then(data) => {
+  console.log(data);}
+
+
 fetch("http://localhost:3000/api/products/")
   .then((res) => {
     if (res.ok) {
@@ -13,14 +22,29 @@ fetch("http://localhost:3000/api/products/")
     cart__article.setAttribute("data-id", obj.Id);
     cart__article.setAttribute("data-color", obj.color);
     items.appendChild(cart__article);
+
+    const itemimg = document.getElementsByClassName("cart__item")[0];
+    const cart__item__img = document.createElement("div");
+    itemimg.appendChild(cart__item__img);
+    cart__item__img.classList.add("cart__item__img");
+
+    for (let i = 0; i <= data.length; i++) {
+      const image = document.getElementsByClassName("cart__item__img")[0];
+      const element_img = document.createElement("img");
+      element_img.src = data[i].imageUrl;
+      element_img.setAttribute("alt", "Photographie d'un canapé");
+      image.appendChild(element_img);
+      console.log("http://localhost:3000/api/products/" + obj.Id);
+    }
   })
   .catch(function (err) {
     // Une erreur est survenue
   });
 
 // Retrieving data:
-let color = localStorage.getItem("Panier");
-let obj = JSON.parse(color);
-/*document.getElementById("data-id").innerHTML = obj.Id;
-document.getElementById("data-color").innerHTML = obj.color;*/
+let quantity = localStorage.getItem("Panier");
+let obj = JSON.parse(quantity);
 document.getElementById("demo").innerHTML = obj.quantity;
+
+document.getElementById("data-id").innerHTML = obj.Id;
+document.getElementById("data-color").innerHTML = obj.color;
